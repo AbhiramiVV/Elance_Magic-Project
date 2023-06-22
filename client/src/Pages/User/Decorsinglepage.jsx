fcf66654c913ff97cb3d26c40f34ebebd2301ec0
Abhiramivv <abhiramivv77@gmail.com>
2023-06-22 10:11:48 +0530

    decorations

--- client/src/Pages/User/Decorsinglepage.jsx
+++ client/src/Pages/User/Decorsinglepage.jsx
@@ -238,11 +238,30 @@ setAmountpay(amountpay)
                       />
               </div>
 
-                   {isExist?<p className="mt-4 bg-black text-white text-xl font-bold py-2 px-12 rounded justify-end">Sorry.Decor team is not available on this date</p>:
-                      <button className="mt-4 bg-black text-white text-xl font-bold py-2 px-12 rounded justify-end"  onClick={() => setModal(!modal)}>
-                        Book Now
-                      </button>
-                      }
+              <div className="flex justify-center items-center mt-4">
+  <button
+    className={`bg-black text-white text-xl font-bold py-2 px-12 rounded mr-2 ${
+      paymentOption === "advance" ? "bg-green-500" : "bg-gray-500"
+    }`}
+    onClick={() => {
+      setPaymentOption("advance");
+      setModal(true); // Add this line to open the payment modal
+    }}
+  >
+    Advance Payment
+  </button>
+  <button
+    className={`bg-black text-white text-xl font-bold py-2 px-12 rounded ml-2 ${
+      paymentOption === "full" ? "bg-green-500" : "bg-gray-500"
+    }`}
+    onClick={() => {
+      setPaymentOption("full");
+      setModal(true); // Add this line to open the payment modal
+    }}
+  >
+    Full Payment
+  </button>
+</div>
             </div>
           </div>
 
