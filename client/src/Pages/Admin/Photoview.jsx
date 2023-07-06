b847a91a0efc7258e6b426eea2c5e760172cb092
Abhiramivv <abhiramivv77@gmail.com>
2023-07-06 16:59:57 +0530

    photo icon

--- client/src/Pages/Admin/Photoview.jsx
+++ client/src/Pages/Admin/Photoview.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from '../../instance/axios'
 import BaseTable from '../../Component/Basetable'
 import { NavLink } from 'react-router-dom'
+import { FaEye, FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
 import Adminsidebar from '../../Component/Adminsidebar'
 import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
@@ -92,17 +93,18 @@ const Photoview=()=> {
       {
           name:"View",
           selector:(row)=>
-          <NavLink to={`/vendor/photosingleview/${row._id}`}><button className='bg-green-900  text-white font-bold py-2 px-4 rounded'><i className='fa fa-eye' /></button></NavLink>
+          <NavLink to={`/vendor/photosingleview/${row._id}`}> <FaEye className="text-green-900" /></NavLink>
 
       },
       {
           name:"Edit",
           selector:(row)=>
-          <NavLink to={`/vendor/photoEdit/${row._id}`}><button className='bg-green-900  text-white font-bold py-2 px-4 rounded'><i className='fa fa-pencil' /></button></NavLink>
+          <NavLink to={`/vendor/photoEdit/${row._id}`}>  <FaPencilAlt className="text-green-900" /></NavLink>
       },
       {
         name:"Remove",
-        selector:(row)=><button onClick={()=>handleDelete(row._id)} className='bg-red-900  text-white font-bold  py-2 px-4 rounded'><i class="fa-solid fa-trash-can"></i></button>
+        selector:(row)=><button onClick={()=>handleDelete(row._id)} className='bg-red-900  text-white font-bold  py-2 px-4 rounded'>
+        <FaTrashAlt /></button>
     },
       
       
