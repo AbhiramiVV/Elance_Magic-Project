7ae5ffc9baa35b2af0c022e8d887b05d968276b1
Abhiramivv <abhiramivv77@gmail.com>
2023-07-06 17:10:58 +0530

    super

--- client/src/routes/Superadmin.js
+++ client/src/routes/Superadmin.js
@@ -3,7 +3,6 @@ import {Routes,Route} from "react-router-dom";
 import Dashboard from '../Pages/Superadmin/Dashboard'
 import Superlogin from '../Pages/Superadmin/Superlogin'
 import Adminview from '../Pages/Superadmin/Adminview';
-import Adminadd from '../Pages/Superadmin/Adminadd';
 import AdminEdit from '../Pages/Superadmin/AdminEdit';
 import Individualview from '../Pages/Superadmin/Individualview';
 import { useAuthContext } from '../Hooks/useAuthContext';
@@ -21,7 +20,6 @@ function Superadmin() {
         <Route path='/dashboard'  element={superadmin ?<Dashboard/>:<Superlogin/>}/>
         <Route path='/customerview'  element={superadmin?<CustomerView/>:<Superlogin/>}/>
         <Route path='/getadmin'  element={superadmin?<Adminview/>:<Superlogin/>}/>
-        <Route path='/addadmin'  element={superadmin?<Adminadd/>:<Superlogin/>}/>
         <Route path='/editadmin/:id'  element={superadmin?<AdminEdit/>:<Superlogin/>}/>
         <Route path='/singleview/:id'  element={superadmin?<Individualview/>:<Superlogin/>}/>
         <Route path='/transaction' element={superadmin?<TransactionTable/>:<Superlogin/>}/>
