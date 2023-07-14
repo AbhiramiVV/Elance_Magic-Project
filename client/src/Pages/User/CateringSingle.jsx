43077b20125408bd11651c894f1efa36b1be12c3
Abhiramivv <abhiramivv77@gmail.com>
2023-07-14 12:57:09 +0530

    catering curosal

--- client/src/Pages/User/CateringSingle.jsx
+++ client/src/Pages/User/CateringSingle.jsx
@@ -11,6 +11,8 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 import chatLogo from "../../assets/chat.png"
@@ -329,39 +331,20 @@ function CateringSingle() {
             </div>
  
   
-            <div class="md:lg:xl:w-1/2 bg-white flex flex-wrap justify-center content-center">
-              <div class="grid grid-cols-2 gap-2 mt-20 mr-8">
-                <div>
-                  <img
-                    class="h-auto max-w-full rounded-lg"
-                    src={`http://localhost:5000/uploads/${image[0]?.files[0]?.filename}`}
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    class="h-auto max-w-full rounded-lg"
-                    src={`http://localhost:5000/uploads/${image[0]?.files[1]?.filename}`}
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    class="h-auto max-w-full rounded-lg"
-                    src={`http://localhost:5000/uploads/${image[0]?.files[2]?.filename}`}
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    class="h-auto max-w-full rounded-lg"
-                    src={`http://localhost:5000/uploads/${image[0]?.files[3]?.filename}`}
-                    alt=""
-                  />
-                </div>
-              </div>
+            <div className="md:lg:xl:w-1/2 bg-white flex flex-wrap justify-end content-center mx-auto">
+            <div className="grid grid-cols-2 gap-2 mt-20 mr-8">
+              <Carousel showThumbs={false}>
+                {image[0]?.files.slice(1).map((file, index) => (
+                  <div key={index}>
+                    <img
+                      src={`http://localhost:5000/uploads/${file.filename}`}
+                      alt={`Carousel Item ${index}`}
+                    />
+                  </div>
+                ))}
+              </Carousel>
             </div>
-  
+          </div>
             {modal && (
               <div className="fixed z-20 inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
                 <div className="bg-white p-2 rounded w-96 m-5">
