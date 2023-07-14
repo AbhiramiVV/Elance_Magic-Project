b2714559595677579133b20aa250960c61deda21
Abhiramivv <abhiramivv77@gmail.com>
2023-07-14 16:39:01 +0530

    cater new chat

--- client/src/Pages/Admin/CateringEdit.jsx
+++ client/src/Pages/Admin/CateringEdit.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-
 import axios from '../../instance/axios'
 import {useNavigate,useParams } from 'react-router-dom';
 import Adminsidebar from '../../Component/Adminsidebar';
