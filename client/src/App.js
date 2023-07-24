ee9d98be1ee235740277b7db62180b2a993eeb2c
Abhiramivv <abhiramivv77@gmail.com>
2023-07-24 09:55:07 +0530

    ene

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import Superadmin from "./routes/Superadmin";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import axios from '././instance/axios'
-import PageNotFound from "./Component/PageNotFound";
 
 
 function App() {
